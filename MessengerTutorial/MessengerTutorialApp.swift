db069b81625f112945a8e56bba5fca5347ec58c0
sasaki <sasaking0215@outlook.jp>
2023-08-09 13:16:53 +0900

    Initial Commit

---
+++ MessengerTutorial/MessengerTutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  MessengerTutorialApp.swift
+//  MessengerTutorial
+//
+//  Created by Mankuki_era on 2023/08/09.
+//
+
+import SwiftUI
+
+@main
+struct MessengerTutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
